8d889c36e85987e8763bb71511874bc0bd76eac0
andries.runkat <andries.runkat@homecredit.co.id>
2019-11-18 14:07:24 +0700

    fetch all location name on lombok

--- runnerCheck.py
+++ runnerCheck.py
@@ -4,16 +4,21 @@ from BeautifulSoup import BeautifulSoup
 import bs4
 
 class Scrappering:
+   'Common base class for all employees'
+   empCount = 0  
+   
+
    def initScrapper(self,surl):
        url = surl
        response = requests.get(url)
-      #  result =[]
        soup = bs4.BeautifulSoup(response.text,'lxml')
-       result = soup.select('.listing_title')
+       print type(soup)
+       result = soup.select('.listing_title > a, .title_with_snippets > a')
+       
        for start in result:
           print start.getText()
 
 
     
 emp1 = Scrappering()
-emp1.initScrapper("https://www.tripadvisor.com/Attractions-g297733-Activities-c57-Lombok_West_Nusa_Tenggara.html#FILTERED_LIST",)
+emp1.initScrapper("https://www.tripadvisor.co.id/Attractions-g297733-Activities-Lombok_West_Nusa_Tenggara.html",)
