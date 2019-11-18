8626c299df326040da8aabcde9375f936b699f7a
andries.runkat <andries.runkat@homecredit.co.id>
2019-11-18 16:00:38 +0700

    add detail scrap

--- runnerCheck.py
+++ runnerCheck.py
@@ -7,18 +7,40 @@ class Scrappering:
    'Common base class for all employees'
    empCount = 0  
    
+   def detailPlace(self, url):
+      response = requests.get("https://www.tripadvisor.co.id"+url)
+      soup = bs4.BeautifulSoup(response.text,'lxml')
+      result = soup.select('.noQuotes')
+      if len(result)==0:
+        return
+
+      for start in result:
+        print start.getText()  
+
+
+
 
    def initScrapper(self,surl):
        url = surl
        response = requests.get(url)
        soup = bs4.BeautifulSoup(response.text,'lxml')
-       print type(soup)
+
+      # find element inside a html tag based on class and element tag with 2 different class
        result = soup.select('.listing_title > a, .title_with_snippets > a')
+
        
        for start in result:
+         #  text for place name
           print start.getText()
+          self.detailPlace(start['href'])
+          print('------------------------------------------')
+          
+         #  link for detail page
+         #  print start["href"]
+
+
 
 
     
 emp1 = Scrappering()
-emp1.initScrapper("https://www.tripadvisor.co.id/Attractions-g297733-Activities-Lombok_West_Nusa_Tenggara.html",)
+emp1.initScrapper("https://www.tripadvisor.co.id/Attractions-g297733-Activities-Lombok_West_Nusa_Tenggara.html")
